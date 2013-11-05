c8390ac8f00d9217555985790d5c1788a9f4c66d
denic <denic@crunch>
2013-11-05 12:36:04 +0300

    Проверка

--- application/views/admin/compare.php
+++ application/views/admin/compare.php
@@ -8,8 +8,9 @@
         function(data){
 			$.each(data, function(i, val) {
 				console.info(data[i].client_name+'_bill25 ==> '+data[i].ostatok);
-				if(source_selector=='file_25'){
+				if(source_selector == 'file_25'){
 					identifier = data[i].client_name+'_bill25';
+					
 				}else{
 					identifier = data[i].client_name;
 				}
