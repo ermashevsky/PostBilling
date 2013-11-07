6d90d008f0fcfa1bdd8b83c6a0941cc35fb8b6fd
denic <denic@crunch>
2013-11-07 11:36:27 +0300

    Запись данных

--- application/models/money_model.php
+++ application/models/money_model.php
@@ -465,13 +465,7 @@ class Money_model extends CI_Model
 					$money -> period = $period;
 					$money -> amount = $row -> amount;
 					$money -> payment = $row -> payment;
-					echo $row->payment;
-					if($row -> payment==''):
-					(double)$money -> postbilling_amount = (double)$row -> amount;
-					else:
 					(double)$money -> postbilling_amount = (double)$row -> amount - (double)$row -> payment;
-					endif;
-
 					$data[$money -> id_account] = $money;
 					$this -> insertCompareDataPostBilling($data);
 				endforeach;
