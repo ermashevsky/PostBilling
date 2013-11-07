13c5611a1cebe29f16d6a462cb32cecb4567b84d
denic <denic@crunch>
2013-11-07 08:18:03 +0300

    Получение данных из PB

--- application/models/money_model.php
+++ application/models/money_model.php
@@ -435,39 +435,40 @@ class Money_model extends CI_Model
 		$ids = $this -> db -> get();
 		$myarr = array();
 		$count = 0;
-		foreach ($ids->result() as $val):
-			$myarr[$count++] = $val->id_account;
+		foreach ($ids -> result() as $val):
+			$myarr[$count ++] = $val -> id_account;
 		endforeach;
 
 		if (0 < $ids -> num_rows) {
-		$this -> db -> select('clients_accounts.bindings_name AS bindings_name, clients_accounts.accounts AS account, clients_accounts.id AS id_account, SUM( customer_payments.amount ) AS amount, IFNULL( ROUND( payment.payments, 2 ) , "00.00" ) AS payment', FALSE);
-		$this -> db -> from('clients');
-		$this -> db -> join('clients_accounts', 'clients_accounts.id_clients =  clients.id', 'left');
-		$this -> db -> join('customer_payments', 'customer_payments.id_account =  clients_accounts.id', 'left');
-		$this -> db -> join('(SELECT * , ROUND( SUM( REPLACE( amount,  "," , "." ) ) , 2 ) AS payments
+			$this -> db -> select('clients_accounts.bindings_name AS bindings_name, clients_accounts.accounts AS account, clients_accounts.id AS id_account, SUM( customer_payments.amount ) AS amount, IFNULL( ROUND( payment.payments, 2 ) , "00.00" ) AS payment', FALSE);
+			$this -> db -> from('clients');
+			$this -> db -> join('clients_accounts', 'clients_accounts.id_clients =  clients.id', 'left');
+			$this -> db -> join('customer_payments', 'customer_payments.id_account =  clients_accounts.id', 'left');
+			$this -> db -> join('(SELECT * , ROUND( SUM( REPLACE( amount,  "," , "." ) ) , 2 ) AS payments
 		FROM customer_encashment
 		GROUP BY id_account
 		) AS payment', 'payment.id_account =  clients_accounts.id', FALSE);
 
-		$this -> db -> where_in('clients_accounts.id', $myarr);
+			$this -> db -> where_in('clients_accounts.id', $myarr);
 
-		$this -> db -> group_by('clients_accounts.accounts');
-		$res = $this -> db -> get();
-		$data = array();
+			$this -> db -> group_by('clients_accounts.accounts');
+			$res = $this -> db -> get();
+			$data = array();
 
 			if (0 < $res -> num_rows) {
-			foreach ($res -> result() as $row):
-				$money = new Money_model();
-				$money -> id_account = $row -> id_account;
-				$money -> bindings_name = $row -> bindings_name;
-				$money -> account = $row -> account;
-				$money -> amount = $row -> amount;
-				$money -> payment = $row -> payment;
-				$data[$money -> id_account] = $money;
-			endforeach;
+				foreach ($res -> result() as $row):
+					$money = new Money_model();
+					$money -> id_account = $row -> id_account;
+					$money -> bindings_name = $row -> bindings_name;
+					$money -> account = $row -> account;
+					$money -> amount = $row -> amount;
+					$money -> payment = $row -> payment;
+					$money -> postbilling_amount = $row -> amount - $row -> payment;
+					$data[$money -> id_account] = $money;
+				endforeach;
 			}
-	}
-			return $data;
+		}
+		return $data;
 	}
 //	SELECT  `id` ,  `id_account` ,  `account` , GROUP_CONCAT(  `identifier` ) ,  `bindings_name` ,  `period` , GROUP_CONCAT(  `source_type` ) , SUM(  `balance` ) AS billings_amount
 //FROM  `compare_balance`
