70d78ddbffd5d0cd5877c2802d2e1d744bf03459
denic <denic@crunch>
2013-11-07 15:33:52 +0300

    Вроде всё работает

--- application/views/admin/compare.php
+++ application/views/admin/compare.php
@@ -4,7 +4,7 @@
 	{	$.blockUI({ message: '<h1><img src="/assets/images/busy.gif" /> Импортируем...</h1>' });
 		period = $('#datepicker').val();
 		source_selector = $('#source_selector').val();
-		$.post('<?=site_url('money/readCSVFile');?>',{'full_path':full_path},
+		$.post('<?= site_url('money/readCSVFile'); ?>',{'full_path':full_path},
         function(data){
 			$.each(data, function(i, val) {
 
@@ -17,7 +17,7 @@
 				}
 				balance = data[i].ostatok;
 
-				$.post('<?=site_url('money/searchAccountByIdentifier');?>',{'identifier':identifier, 'balance': balance, 'period': period, 'source_selector': source_selector},
+				$.post('<?= site_url('money/searchAccountByIdentifier'); ?>',{'identifier':identifier, 'balance': balance, 'period': period, 'source_selector': source_selector},
 				function(data){
 					console.info(data);
 				},'json');
@@ -30,7 +30,7 @@
 	function getPostBillingData()
 	{
 		period = $('#datepicker_postbilling').val();
-		$.post('<?=site_url('money/getPostBillingData');?>',{'period': period},
+		$.post('<?= site_url('money/getPostBillingData'); ?>',{'period': period},
         function(data){
 			console.info(data);
 		},'json');
@@ -38,42 +38,42 @@
 	}
 
 	function buildCompareDataTable(){
-				$('#compareData').empty();
+		$('#compareData').empty();
 
-				$.post('<?php echo site_url('/money/buildCompareDataTable'); ?>',
-				function(data){
-					console.info(data)
-					$('#compareData').append('<table  id="DataTable" class="table_wrapper_inner"><thead><th>ЛС</th><th>Идентификаторы</th><th>Клиент</th><th>Период</th><th>Остаток по биллингу</th><th>Остаток по постбиллингу</th></thead><tbody></tbody></table>');
-					$.each(data, function(i, val) {
-						$('#DataTable').append('<tr><td>'+data[i].account+'</td><td>'+data[i].identifier+'</td><td>'+data[i].bindings_name+'</td><td>'+data[i].period+'</td><td>'+data[i].billings_amount+'</td><td>'+data[i].postbilling_amount+'</td></tr>');
-					});
+		$.post('<?php echo site_url('/money/buildCompareDataTable'); ?>',
+		function(data){
+			console.info(data)
+			$('#compareData').append('<table  id="DataTable" class="table_wrapper_inner"><thead><th>ЛС</th><th>Идентификаторы</th><th>Клиент</th><th>Период</th><th>Остаток по биллингу</th><th>Остаток по постбиллингу</th></thead><tbody></tbody></table>');
+			$.each(data, function(i, val) {
+				$('#DataTable').append('<tr><td>'+data[i].account+'</td><td>'+data[i].identifier+'</td><td>'+data[i].bindings_name+'</td><td>'+data[i].period+'</td><td>'+data[i].billings_amount+'</td><td>'+data[i].postbilling_amount+'</td></tr>');
+			});
 
-					oTable = $('#DataTable').dataTable({
-						"aaSorting": [[0, 'asc']],
-						"bJQueryUI": false,
-						"bProcessing":true,
-						"sPaginationType": "full_numbers",
-						"oLanguage": {
-							"sUrl": "/assets/admin/js/russian-language-DataTables.txt"
-						},
-						"bAutoWidth": true,
-						"bDestroy": true,
-						"sScrollY": "320px",
-						"sDom": 'T<"clear">lfrtip',
-						"oTableTools": {
-							"aButtons": [
-								{
-									"sExtends": "csv",
-									"sButtonText": "Сохранить в CSV"
-								}
-							],
-							"sSwfPath": "/assets/admin/swf/copy_csv_xls_pdf.swf"
+			oTable = $('#DataTable').dataTable({
+				"aaSorting": [[0, 'asc']],
+				"bJQueryUI": false,
+				"bProcessing":true,
+				"sPaginationType": "full_numbers",
+				"oLanguage": {
+					"sUrl": "/assets/admin/js/russian-language-DataTables.txt"
+				},
+				"bAutoWidth": true,
+				"bDestroy": true,
+				"sScrollY": "320px",
+				"sDom": 'T<"clear">lfrtip',
+				"oTableTools": {
+					"aButtons": [
+						{
+							"sExtends": "csv",
+							"sButtonText": "Сохранить в CSV"
 						}
-					});
-					$('#compareData').show();
-				},'json');
+					],
+					"sSwfPath": "/assets/admin/swf/copy_csv_xls_pdf.swf"
+				}
+			});
+			$('#compareData').show();
+		},'json');
 
-			}
+	}
 
 </script>
 
@@ -109,19 +109,19 @@
 					echo '</div>';
 				} else {
 					?>
-				<div style="margin:10px;">
-					<p style="font-size: large; color:#79C20D;">В таблице содержится
-					<?php
-						$get = new Admin();
-						$get -> getCountRow();
-					?>
-					записей.</p>
-					<?php echo form_open_multipart('upload/do_upload_sverka', 'id=form2'); ?>
-					<input type="hidden" name="folder" size="20" value="sverka"/>
-					<input type="file" name="userfile" size="20"/>
-					<input type="submit" value="Загрузить" />
-					</form>
-				</div>
+					<div style="margin:10px;">
+						<p style="font-size: large; color:#79C20D;">В таблице содержится
+							<?php
+							$get = new Admin();
+							$get -> getCountRow();
+							?>
+							записей.</p>
+						<?php echo form_open_multipart('upload/do_upload_sverka', 'id=form2'); ?>
+						<input type="hidden" name="folder" size="20" value="sverka"/>
+						<input type="file" name="userfile" size="20"/>
+						<input type="submit" value="Загрузить" />
+						</form>
+					</div>
 					<?php
 				}
 				?>
@@ -131,26 +131,26 @@
 				<label>Сбор и анализ данных</label><br/><br/>
 				<div name="divContainer" style="float:left; margin:10px;">
 					<p style="font-size: large; color:#79C20D;">В таблице содержится
-					<?php
+						<?php
 						$get = new Admin();
 						$get -> getCountRowPostBillingData();
-					?>
-					записей.</p>
+						?>
+						записей.</p>
 					<p>
 
-					<input type="text" id="datepicker_postbilling" style="width:80px;" name="datepicker_postbilling"/>
+						<input type="text" id="datepicker_postbilling" style="width:80px;" name="datepicker_postbilling"/>
 
-					<input type="button" name="getDataSet" id="getDataSet" value="Получить данные" onclick="getPostBillingData();"/>
+						<input type="button" name="getDataSet" id="getDataSet" value="Получить данные" onclick="getPostBillingData();"/>
 					</p>
 				</div>
 			</fieldset>
 			<fieldset>
 				<label>Сводная таблица остатков</label>
 			</fieldset>
-			
-					<div id="compareData" style="display:none;">
 
-					</div>
+			<div id="compareData" style="display:none;">
+
+			</div>
 
 
 			<div class="clear"></div>
@@ -163,22 +163,8 @@
 	</article><!-- end of content manager article -->
 
 	<div class="clear"></div>
-
-	<h4 class="alert_warning">A Warning Alert</h4>
-
-	<h4 class="alert_error">An Error Message</h4>
-
-	<h4 class="alert_success">A Success Message</h4>
 	<div class="spacer"></div>
 </section>
-
-<div id="dialog-confirm" title="Пакетная смена тарифного плана">
-
-</div>
-<div id="dialog-confirm-ok" title="Пакетная смена тарифного плана">
-
-</div>
-
 
 </body>
 
