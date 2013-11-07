7624ee1a2a8f95badaeae36958a3a8ac35a50d0b
denic <denic@crunch>
2013-11-07 15:15:56 +0300

    Сводная таблица - представление

--- application/views/admin/compare.php
+++ application/views/admin/compare.php
@@ -43,12 +43,12 @@
 				$.post('<?php echo site_url('/money/buildCompareDataTable'); ?>',
 				function(data){
 					console.info(data)
-					$('#compareData').append('<table  id="DataTable" class="table_wrapper_inner"><thead><th>Лицевой счет</th><th>Номенклатура</th><th>Количество</th><th>Цена</th><th>Сумма</th></thead><tbody></tbody></table>');
+					$('#compareData').append('<table  id="DataTable" class="table_wrapper_inner"><thead><th>Лицевой счет</th><th>Идентификаторы</th><th>Наименование клиента</th><th>Период</th><th>Остаток по биллингу</th><th>Остаток по постбиллингу</th></thead><tbody></tbody></table>');
 					$.each(data, function(i, val) {
-						$('#DataTable').append('<tr><td>'+data[i].account+'</td><td>'+data[i].identifier+'</td><td>'+data[i].bindings_name+'</td><td>'+data[i].billings_amount+'</td><td>'+data[i].postbilling_amount+'</td></tr>');
+						$('#DataTable').append('<tr><td>'+data[i].account+'</td><td>'+data[i].identifier+'</td><td>'+data[i].bindings_name+'</td><td>'+data[i].period+'</td><td>'+data[i].billings_amount+'</td><td>'+data[i].postbilling_amount+'</td></tr>');
 					});
 
-					oTable = $('#reportDataTable').dataTable({
+					oTable = $('#DataTable').dataTable({
 						"aaSorting": [[0, 'asc']],
 						"bJQueryUI": false,
 						"bProcessing":true,
@@ -70,7 +70,7 @@
 							"sSwfPath": "/assets/admin/swf/copy_csv_xls_pdf.swf"
 						}
 					});
-					$('#report1C').show();
+					$('#compareData').show();
 				},'json');
 
 			}
