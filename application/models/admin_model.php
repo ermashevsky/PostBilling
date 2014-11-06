8ee814ff59a2333f7245a1145dbcc342baaf2276
Denic <ermashevsky@gmail.com>
2014-11-06 12:20:35 +0300

    Fatal error: Uncaught exception 'Exception' with message 'DateTime::__construct(): It is not safe to rely on the system's timezone settings.

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -14,7 +14,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
+date_default_timezone_set('Europe/Kaliningrad');
 /**
  * Класс Admin содержит методы админки
  *
