58dd033bdace4fe9ea6e21380158daf00d80cf21
Denic <ermashevsky@gmail.com>
2015-04-01 11:21:06 +0300

    Высота таблицы содержимого ЛС клиента уменьшена с помощью css параметр height = 150 px

--- application/views/client_accounts.php
+++ application/views/client_accounts.php
@@ -1328,7 +1328,9 @@ $('button#add_account_button2.add_account_button').click(function(){
     #map{
         display:none;
     }
-	div#example_wrapper .dataTables_wrapper{
+	#example_wrapper{
+		position: relative;
+		clear: both;
 		height:150px;
 	}
 
