6178f04a078a33979a44efeb811ffcb09fe8b0ac
Denic <ermashevsky@gmail.com>
2015-06-17 10:16:33 +0300

    7.2/ Разработка

--- application/models/clients_model.php
+++ application/models/clients_model.php
@@ -1013,8 +1013,6 @@ class Clients_model extends CI_Model
 	 */
 	function getElementsForm($id, $id_client)
 	{
-		echo $id;
-		echo $id_client;
 		$elements_arr = array();
 		$this -> db -> select('service_groups.id, service_groups.services_groups, assortment.id as assortment_id, payment_name, payment_type, element_type, target, default_value,type_resources,tariff');
 		$this -> db -> from('assortment');
